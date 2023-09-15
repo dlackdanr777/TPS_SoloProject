07b96afee26518fac93db015831a1842f84fbfed
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2023-09-16 01:28:40 +0900

    업로드
    빌딩시스템 추가 구축 파일 업로드

--- Assets/Scripts/PreviewObject.cs
+++ Assets/Scripts/PreviewObject.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PreviewObject : MonoBehaviour
 {
-    [SerializeField] private GameObject _previewPrefab;
-    [SerializeField] private GameObject _buildPrefab;
     [SerializeField] private Material _greenMaterial;
     [SerializeField] private Material _redMaterial;
-    private void OnTriggerStay(Collider other)
+    private List<Collider> _colliders = new List<Collider>();
+    private const int IGNORE_RAYCAST_LAYER = 2;
+
+    private void Update()
     {
-        if(other.gameObject.layer != LayerMask.NameToLayer("Ground"))
+        ChangeColor();
+
+    }
+
+    private void ChangeColor()
+    {
+        if (_colliders.Count > 0)
+            SetColor(_redMaterial);
+        else
+            SetColor(_greenMaterial);
+    }
+
+    private void SetColor(Material mat)
+    {
+        transform.GetComponent<Renderer>().material = mat;
+        foreach (Transform tfChild in transform)
         {
-            _previewPrefab.tr
+            Material[] newMaterials = new Material[tfChild.GetComponent<Renderer>().materials.Length];
+
+            for (int i = 0, count = newMaterials.Length; i < count; i++)
+                newMaterials[i] = mat;
+
+            tfChild.GetComponent<Renderer>().materials = newMaterials;
         }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.gameObject.layer != 9 && other.gameObject.layer != 17 && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+        {
+            Debug.Log(other.gameObject.layer);
+            _colliders.Add(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != 9 && other.gameObject.layer != 17 &&  other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+        {
+            _colliders.Remove(other);
+        }
+    }
+
+    public bool isBuildable()
+    {
+        return _colliders.Count == 0;
     }
 }
